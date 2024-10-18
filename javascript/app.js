f8fe102d5f6f13718fb1b5c47b94a0f5e52b9a04
Haris <mharis@tibxtech.com>
2024-10-18 23:02:07 +0430

    Enhanced the code and added new features

--- javascript/app.js
+++ javascript/app.js
@@ -10,29 +10,44 @@ const moonPhases = [
 ]
 
 const moonCycleDays = 29.5
-const newMoonDate = new Date(2024, 0, 12)
+const newMoonDate = new Date(2024, 0, 10)
 
 let currentMonth = new Date().getMonth()
 let currentYear = new Date().getFullYear()
 
-// Create Calendar ----------------------------------------------------------------
+let selectedDay = new Date().getDate()
+
+// Calendar ----------------------------------------------------------------
+
 function createCalendar(month, year) {
     let calendarGrid = document.getElementById("calendar-grid")
     const monthsDays = new Date(year, month + 1, 0).getDate()
-    let DivElement = ``
+    let divElements = ``
     for (let day = 1; day <= monthsDays; day++) {
-        DivElement += `
-            <div onclick="handleDaySelect(${day})"> ${day} </div>
+        divElements += `
+            <div 
+                class="${selectedDay === day ? 'selected' : ''}" 
+                onclick="handleDaySelect(${day})">${day}
+            </div>
         `
     }
-    calendarGrid.innerHTML = DivElement
+    calendarGrid.innerHTML = divElements
+
     document.getElementById("current-month").innerText = new Date(year, month).toLocaleString('en-US', {
         year: '2-digit',
         month: 'long'
     })
 }
 
-function prevMonth(){
+function handleDaySelect(day) {
+    selectedDay = day
+    const date = new Date(currentYear, currentMonth, day)
+    const moonIndex = getMoonPhaseIndex(date)
+    displayMoonPhase(date, moonIndex)
+    createCalendar(currentMonth, currentYear)
+}
+
+function prevMonth() {
     currentMonth--
     if (currentMonth < 0) {
         currentMonth = 11
@@ -41,48 +56,50 @@ function prevMonth(){
     createCalendar(currentMonth, currentYear)
 }
 
-function nextMonth(){
+function nextMonth() {
     currentMonth++
-    if(currentMonth > 11){
+    if (currentMonth > 11) {
         currentMonth = 0
         currentYear++
     }
     createCalendar(currentMonth, currentYear)
 }
 
-function handleDaySelect(day) {
-    const modifiedDate = new Date(2024, 9, day)
-    const index = getMoonPhaseIndex(modifiedDate)
-    displayMoon(modifiedDate , index)
-}
 
-// Main function ----------------------------------------------------------------
+// Display Moon Phase
 function getMoonPhaseIndex(date) {
     const daysSinceNewMoon = Math.floor((date - newMoonDate) / (1000 * 60 * 60 * 24))
-    const moonAge = daysSinceNewMoon % moonCycleDays
+    const moonAge = Math.floor(daysSinceNewMoon % moonCycleDays)
     const index = Math.floor((moonAge / moonCycleDays) * moonImages.length)
     return index
 }
 
-
-function showCurrentMoonPhase() {
-    const today = new Date()
-    const index = getMoonPhaseIndex(today)
-    displayMoon(today , index)
-}
-
-
-function displayMoon(date , index){
-    document.getElementById("moon-image").src = `./images/${moonImages[index]}`
-    document.getElementById("moon-phase-name").innerText = moonPhases[Math.floor(index / 2)]
-    document.getElementById("selected-date").innerText = date.toLocaleString('en-US', {
+function displayMoonPhase(date, index) {
+    let img = document.getElementById("moon-image")
+    img.style.opacity = 0;
+    setTimeout(() => {
+        img.src = `./images/${moonImages[index]}`
+        img.onload = () => {
+            img.style.opacity = 1
+        }
+    }, 500)
+    document.getElementById("moon-phase-name").textContent = moonPhases[Math.floor(index / 2)]
+    document.getElementById("selected-date").textContent = date.toLocaleString('en-US', {
         year: 'numeric',
         month: 'long',
         day: 'numeric'
     })
 }
 
+function showCurrentPhase() {
+    const today = new Date()
+    const index = getMoonPhaseIndex(today)
+    displayMoonPhase(today, index)
+}
 
-
+showCurrentPhase()
 createCalendar(currentMonth, currentYear)
-showCurrentMoonPhase()
+
+window.onload = () => {
+    document.body.style.height = `${window.innerHeight}px`
+}
